f9e6d432dd9282c5fc0d770d713057413a14dad9
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-01-10 09:06:14 +0100

    Start style

--- index.php
+++ index.php
@@ -13,8 +13,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title><?php if ($lang == "fr") { echo "Coucou, je suis Cut0x !"; } else { echo "Hi, I'm Cut0x !"; }; ?></title>
+    <title>
+	<?php if ($lang == "fr") {
+		echo "Coucou, je suis Cut0x !";
+	} else {
+		echo "Hi, I'm Cut0x !";
+	}; ?>
+	</title>
+	
+	<link rel="stylesheet" href="./src/css/style.css"/>
 </head>
 <body>
+	<a href="#" class="btn">Test</a>
 </body>
 </html>

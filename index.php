532db323c75f12d6457ffefbd55d171157007287
Cut0x <loicvalence1@gmail.com>
2023-01-10 22:12:23 +0100

    up

--- index.php
+++ index.php
@@ -11,12 +11,10 @@
 	$titre_fr = "Coucou, je suis Cut0x !";
 	$titre_en = "Hi, I'm Cut0x !";
 	// NavBar
-	$home_fr = "Acceuil";
-	$home_en = "Home";
 	$projects_fr = "Projets";
 	$projects_en = "Projects";
-	$_fr = "";
-	$_en = "";
+	$login_fr = "Connexion";
+	$login_en = "Log In";
 	// Bouton
 	$pass_fr = "Passer en Anglais";
 	$pass_en = "Switch to French";
@@ -51,22 +49,31 @@
             <ul class="nav-list">
                 <li><a href="#">Contact</a></li>
                 <li><a href="#">Blog</a></li>
-                <li><a href="#">Projets</a></li>
-                <li><a href="./auth/login/">Login</a></li>
+                <li><a href="#"><?php if ($lang == "fr") { echo $projects_fr; } else { echo $projects_en; }; ?></a></li>
+                <li><a href="./auth/login/?lang=<?= $lang; ?>"><?php if ($lang == "fr") { echo $login_fr; } else { echo $login_en; }; ?></a></li>
             </ul>
         </nav>
     </header>
 
     <div style="margin: 20px;"></div>
-    
+
     <div style="text-align: center;">
         <a href="./?lang=<?php if ($lang == "fr") { echo "en"; } else { echo "fr"; }; ?>" class="btn"><?php if ($lang == "fr") { echo $pass_fr; } else { echo $pass_en; }; ?></a>
     </div>
     
     <div style="margin: 20px;"></div>
 
-    <main>
-    </main>
+    <div class="container">
+        <div class="obj">
+            <img src="./src/img/my-logo.png" alt="Logo Cut0x">
+        </div>
+
+        <div class="obj">
+            <h1>
+                <span style="color: #5865F2;">Cut0x</span>#<span style="color: #5865F2;">0001</span>
+            </h1>
+        </div>
+    </div>
     <script src="./src/js/mobile-navbar.js"></script>
 </body>
 </html>

3930498a233e28e4067efb407675cbf437de02d5
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-01-23 16:20:36 +0100

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,3 @@
-console.log('Pourquoi tu regarde mon code source ?')
+console.log('Pourquoi tu regarde mon code source ?\nRejoins mon discord : https://discord.gg/');
+console.warn('Ne pas copier mon code ! Cette version n\'est pas opensource !');
+console.info('Version opensource : https://github.com/Cut0x/');

5ffad7a020636af78a6215bbff68d608d8cf4de4
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-03-14 18:53:33 +0100

    Initialisation des frameworks

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+
+    <title>Cut0x's Website</title>
+
+    <meta name="description" content="Bienvenue sur la page web de Loïc alias Cut0x !">
+
+    <link rel="shortcut icon" href="" type="image/x-icon">
+    <link rel="stylesheet" href="./src/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 </head>
 <body>
     <!-- -->

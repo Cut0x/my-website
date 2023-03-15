01f5d90f5ccd279a508928bd476a4f48d0f6977f
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-03-15 22:45:03 +0100

    Update and import all page

---
+++ src/includes/page/no-page.php
@@ -0,0 +1,17 @@
+<div id="main">
+    <div id="container-no-page">
+        <h1>
+            <i class="bi bi-backspace-reverse-fill"></i> An error has been detected !
+        </h1>
+
+        <p>
+            No page is linked to the identifier "<span style="color: orangered;"><?= $_GET['page']; ?></span>" !
+        </p>
+
+        <center>
+            <a href="./?page=home">
+                Go home
+            </a>
+        </center>
+    </div>
+</div>

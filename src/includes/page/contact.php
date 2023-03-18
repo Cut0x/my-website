2c49e29a06dbe0ad5628ea2b56b5e0de13fd6331
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-03-18 17:21:33 +0100

    Create contact page with css

--- src/includes/page/contact.php
+++ src/includes/page/contact.php
@@ -1,15 +1,20 @@
 <div id="main">
     <div id="container-contact">
         <div id="object-contact">
+            <h1>My social networks</h1>
             <div class="link">
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
+                <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                <a href="#" class="discord"><i class="bi bi-discord"></i></a>
+                <a href="#" class="gmail"><i class="bi bi-envelope-fill"></i></a>
+                <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
             </div>
         </div>
+        
         <div id="object-contact">
-            <form method="post"></form>
+            <h1>Send me a mail</h1>
+            <form method="post">
+                <label for="">test</label>
+            </form>
         </div>
     </div>
 </div>

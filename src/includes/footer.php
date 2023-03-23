4979cf80c7f00e30b081501c023ab1992342589c
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-03-23 13:51:34 +0100

    Update footer

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -2,6 +2,6 @@
 
 <div id="footer">
     <div class="inner">
-        test
+        Developed with <span style="color: red;"><i class="bi bi-suit-heart-fill"></i></span> by <span style="color: #5865F2;">Loïc</span> <i>(aka <span style="color: #5865F2;">Cut0x</span>)</i> !
     </div>
 </div>

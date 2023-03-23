cac5be46480c7904a0ec2dc8aa55320c11e23d25
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-03-23 14:00:05 +0100

    Add home page + Update footer

---
+++ src/includes/page/home.php
@@ -0,0 +1,5 @@
+<div id="main">
+    <div id="container-home">
+        <p>Welcome to my webpage ! I invite you to check out my <a href="./?page=projects">projects</a> and <a href="./?page=about">my profile</a> !</p>
+    </div>
+</div>
